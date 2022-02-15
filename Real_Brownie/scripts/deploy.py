aaf5b0df4c31ae404d28fdadc50d54567a673ee4
prompt2k2 <prompt2k2@gmail.com>
2022-02-15 15:03:03 +0100

    browniee yee

--- Real_Brownie/scripts/deploy.py
+++ Real_Brownie/scripts/deploy.py
@@ -1,8 +1,12 @@
 from brownie import accounts
+import os
 
 
 def deploy_simple_storage():
-    account = accounts[0]
+    # account = accounts.load("prosper-account") #best way to keep private key methond 1
+    # print(account)
+    
+    account = accounts.add(os.environ.get("PRIVATE_KEY"))
     print(account)
 
 
